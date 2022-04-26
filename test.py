232ee85c32489359af17ba0bcdfbf5cce2bdd6c2
unknown <kevinboucly9@gmail.com>
2022-04-26 12:02:50 +0200

    proche voisin

---
+++ test.py
@@ -0,0 +1,7 @@
+from tsp_graph_init import Graph, Lieu
+
+
+lieu = Lieu(50,100)
+lieu2 = Lieu(100,100)
+graph = Graph([lieu])
+graph()
--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -10,16 +10,40 @@ class Lieu :
         return distance
 
 class Graph :
-    def __init__(self) :
-        liste_lieux = []
+    def __init__(self,liste_lieux) :
+        self.liste_lieux = liste_lieux
         largeur = 800
         hauteur = 600
         nb_lieux = 20
         matrice_od = []
 
     def calcul_matrice_cout_od(self):
-        lieux = self.liste_lieux
+        lieux = len(self.liste_lieux)
+        distance_matrice = np.ones((lieux,lieux))
+        for i,lieu1 in enumerate(self.liste_lieux):
+            for j, lieu2 in enumerate(self.liste_lieux):
+                distance_matrice[i, j] = lieu1.distance_euclidienne(lieu2)
+
+        self.matrice_cout_od = distance_matrice
+
+        return distance_matrice
+
+    def plus_proche_voisin(self,lieu,matrice_od):
+        self.le_plus_proche_voisin = np.argmin(matrice_od[lieu])
+        return self.le_plus_proche_voisin
         
-        for i in range(len(self.coordonnee_X)):
-            for j in range(len(self.coordonnee_X)):
-                r[i, j] = self.distance_euclidienne()
+
+
+
+
+
+
+lieu = Lieu(50, 100)
+
+lieu2 = Lieu(100, 100)
+
+dist = lieu.distance_euclidienne(lieu2)
+
+graph = Graph([lieu,lieu2])
+mat = graph.calcul_matrice_cout_od()
+print("ok")
